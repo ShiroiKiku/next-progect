80dbed919bf48decb865846641a9a81a99df11a1
ShiroiKiku <ividevil@mail.ru>
2021-08-12 17:32:36 +0400

    master

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 const {Router} = require('express')
 const router = Router()
 
+
 router.get('/', (req, res) =>{
-    res.render('index',{
-        title:'title'
+    res.render(__filename,{
+        title:'__filename'
     })
 })
-router.get('/about', (req, res) =>{
-    res.render('about',{
-        title:'about'
-    })
-})
+// router.get('/about', (req, res) =>{
+//     res.render('about',{
+//         title:'about'
+//     })
+// })
 
 module.exports = router
